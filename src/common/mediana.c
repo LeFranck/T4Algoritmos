83f47602204818e205fdfb21c94246a929b5ffe7
FrancoCF <franco.alfaro@comunidadfeliz.cl>
2020-03-26 11:35:53 -0300

    fixing

--- src/common/mediana.c
+++ src/common/mediana.c
@@ -10,14 +10,16 @@ int** calcular_mediana(int** L, double n1)
 {
 	//el primer int dira si hay resultado
 	//y el segundo nos dara dicho resultado
+  int debug = 0;
+  time_t t;
+  srand((unsigned) time(&t));
 	int** retorno = malloc(sizeof(int*)*2);
 	int n = round(n1);
-	if(n1 < 145.0)
+	if(n1 < 2001.0)
 	{
 		int** l1 = mergesort(L,n);
 		retorno[0] = 1;
 		int uuu = round(ceil(n1/2));
-		printf("%d\n", uuu);
 		retorno[1] = l1[uuu-1];
 		//retorno[1] = l1[(n/2)+n%2];
 		return retorno;
@@ -27,17 +29,23 @@ int** calcular_mediana(int** L, double n1)
 		int u_index = get_u_index(n1);
 		int** R = malloc(sizeof(int*)*largo);
 		int i = 0;
-		printf("largo: %d \t d_index %d\t u:index  %d\n", largo, d_index, u_index );
+		if(debug){printf("largo: %d \t d_index %d\t u:index  %d\n", largo, d_index, u_index );}
+
 		for(i = 0; i < largo; i++)
 		{
 			int j = rand() % n;
 			R[i] = L[j];
-			printf("|%d|",j);
 		}
-		int** r2 = mergesort(R,largo);
+		
+    int** r2 = mergesort(R,largo);
 		R = r2;
 		free(r2);
-		int d = R[d_index];
+		if(debug)
+    {
+      print_l(R, largo);
+      printf("\nLista ordenada ^^^^^\n");
+    }
+    int d = R[d_index];
 		int u = R[u_index];
 		int md = 0;
 		int mu = 0;
@@ -45,17 +53,19 @@ int** calcular_mediana(int** L, double n1)
 
 		for(i = 0; i < n; i++)
 		{
-			if(d <= L[i] && L[i] <= u)
-			{
+      int aux = L[i];
+			if((d <= aux) && (aux <= u)){
 				largo_s++;
-			}else if(L[i] < d){
+			}else if(aux < d){
 				md++;
+			}else if(aux > u){
+        mu++;
 			}else{
-				mu++;
-			}
+        if(debug){printf("WTF\n");}
+      }
 		}
-
-		if( (md >= n/2 + n%2) || (mu >= n/2 + n%2) || (largo_s >= 4*largo) )
+    int cota = round(floor(pow(n,0.75)));
+    if( (md >= n/2 + n%2) || (mu >= n/2 + n%2) || (largo_s >= 4*cota) )
 		{
 			if(md >= n/2 + n%2)
 			{
@@ -67,9 +77,9 @@ int** calcular_mediana(int** L, double n1)
 				printf("Y2\n");
 			}
 
-			if(largo_s >= 4*largo)
+			if(largo_s >= 4*cota)
 			{
-				printf("%d > %d S\n",largo_s, 4*largo);
+				printf("%d > %d S\n",largo_s , 4*cota);
 			}
 
 			retorno[0] = 0;
@@ -80,13 +90,23 @@ int** calcular_mediana(int** L, double n1)
 			int k = 0;
 			for(i = 0; i < n; i++)
 			{
-				if(d <= L[i] && L[i] <= u)
+        int aux2 = L[i];
+				if((d <= aux2) && (aux2 <= u))
 				{
-					S[k] = L[i];
+					S[k] = aux2;
 					k++;
 				}
 			}
 			int** s2 = mergesort(S,largo_s);
+      S = s2;
+      free(s2);
+      if(debug)
+      {
+        print_l(S, largo_s);
+        printf("\nLista ordenada ^^^^^\n");
+      }
+      printf("largo de s %d\n", largo_s );
+      printf("d y u: %d y %d \n", d, u );
 			retorno[0] = 1;
 			int index_final = n/2 + n%2 - md;
 			retorno[1] = S[index_final];
@@ -98,20 +118,20 @@ int** calcular_mediana(int** L, double n1)
 
 int get_d_index(double n)
 {
-	double r1 =  pow(n,0.6394)/2.0 - pow(n,0.5);
-	int r = round(r1);
-	return r;
+	double d1 =  pow(n,0.75)/2.0 - pow(n,0.5);
+	int d = round(floor(d1));
+	return d;
 }
 
 int get_u_index(double n)
 {
-	double u_index1 = pow(n,0.6394)/2.0 + pow(n,0.5);
-	int r = round(ceil(u_index1));
-	return r;
+	double u_index1 = pow(n,0.75)/2.0 + pow(n,0.5);
+	int u_index = round(ceil(u_index1));
+	return u_index;
 }
 
 int get_largo_R(double n)
 {
-	int r =  round(ceil(pow(n,0.6394)));
+	int r =  round(ceil(pow(n,0.75)));
 	return r;
 }
--- src/solver/solver.c
+++ src/solver/solver.c
@@ -8,23 +8,24 @@ int main(int argc, int *argv[])
 {
 	if(argc == 2)
 	{
-		//Entero* e = init_entero(strlen(argv[1]),argv[1]);
 		Input* input = create_input(argv[1]);
 
-		print_input(input);
-		printf("ahora ordenare con mergesort\n");
-		input->L = mergesort(input->L,input->cantidad);
-		print_input(input);
+		//print_input(input);
+		//printf("ahora ordenare con mergesort\n");
+		//input->L = mergesort(input->L,input->cantidad);
+		//print_input(input);
 		
 		int c = input->cantidad;
 		double c2 = (double)c;
+    int contador = 1;
 		int** resultado = calcular_mediana(input->L,c2);
-		if(resultado[0] == 0)
-		{
-			printf("sin resultado\n");
-		}else{
-			printf("La mediana es %d\n", resultado[1]);
-		}
+    while(resultado[0] == 0)
+    {
+      int** resultado = calcular_mediana(input->L,c2);
+      contador = contador + 1;
+    }
+		printf("La mediana es %d\n", resultado[1]);
+    printf("El algoritmo tuvo que correr %d veces\n",contador);
 		destroy_input(input);
 	}else{
 		Input* input = create_input(argv[1]);
